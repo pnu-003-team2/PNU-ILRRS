0f0b00dc635340f52cd19b88ff8f760d9ae2728c
이교원 <cjswoakzld@naver.com>
2019-04-02 19:53:28 +0900

    구구가가

--- android/app/src/main/java/com/pnuilrrs/MainActivity.java
+++ android/app/src/main/java/com/pnuilrrs/MainActivity.java
@@ -10,6 +10,7 @@ public class MainActivity extends ReactActivity {
     /**
      * Returns the name of the main component registered from JavaScript.
      * This is used to schedule rendering of the component.
+     * branch testtest
      */
     @Override
     protected String getMainComponentName() {
@@ -21,6 +22,7 @@ public class MainActivity extends ReactActivity {
     return new ReactActivityDelegate(this, getMainComponentName()) {
         @Override
         protected ReactRootView createRootView() {
+
         return new RNGestureHandlerEnabledRootView(MainActivity.this);
         }
     };
