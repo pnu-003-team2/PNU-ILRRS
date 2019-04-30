f7d58ea8c00625613ff856664602a9febe840f68
chaangmeen <1008jcm@gmail.com>
2019-04-30 16:44:37 +0900

    홈화면 커스텀 작업 (#11)
    * 강의목록 Flat list 뷰
    
    * 강의 목록 FlatList 커스텀

--- components/CourseList.js
+++ components/CourseList.js
@@ -2,6 +2,19 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { FlatList, StyleSheet, Text, View } from 'react-native';
 
+
+function SepearatedText({ timeTable }) {
+  let [firstLine,secondLine] = timeTable.split(',',2);
+  
+  return (
+    <View>
+      <Text>{firstLine}</Text>
+      <Text>{secondLine}</Text>
+    </View>
+  );
+}
+
+
 const propTypes = {
   courses: PropTypes.arrayOf(PropTypes.shape({
     className: PropTypes.string,
@@ -22,27 +35,65 @@ function CourseList({ courses }) {
       data={courses.map(course => ({ ...course, key: course.id.toString() }))}
       renderItem={({ item }) => (
         <View style={styles.course}>
-          <Text>{item.className} ({item.code}분반) / {item.classDivision} / {item.professorName}</Text>
-          <Text>{item.timeTable}</Text>
+          <View style={styles.coureLeftView}>
+            <Text style={[styles.titleText, styles.bottomPadding]}>{item.className}</Text>
+            <Text style={item.classDivision.includes('전공') ? [styles.redText,styles.bottomPadding] : [styles.grayText,styles.bottomPadding]}>{item.classDivision}</Text>
+            <Text style={[styles.codeText]}>{item.code} 분반</Text>
+            <SepearatedText timeTable={item.timeTable}/>
+          </View >
+          <View style={styles.courseRightView}>
+            <Text style={styles.professorNameText}>{item.professorName} 교수님</Text>
+          </View>
         </View>
       )}
     />
   );
 }
 
+
 const styles = StyleSheet.create({
   course: {
-    padding: 10,
+    flexDirection: 'row',
+    alignItems: 'stretch',
+    padding: 16,
     borderRadius: 5,
     borderWidth: 1,
-    borderColor: 'gray',
+    borderColor: 'skyblue',
     marginLeft: 10,
     marginRight: 10,
     marginBottom: 10,
+  },
+  coureLeftView: {
+    flex: 3,
+    flexDirection: 'column',
+  },
+  courseRightView:{
+    flex: 1,
+
+  },
+  professorNameText: {
+    fontSize: 13,
+  },
+  titleText: {
+    fontWeight: '600',
+    fontSize: 19,
+  },
+  bottomPadding: {
+    paddingBottom : 16,
+  },
+  redText: {
+    color:  'red',
+  },
+  grayText: {
+    color:  'gray',
+  },
+  codeText: {
+    color : 'gray',
+    paddingBottom: 4,
   },
 });
 
 CourseList.propTypes = propTypes;
 CourseList.defaultProps = defaultProps;
-
 export default CourseList;
+
