614259a971ad961a44e627fd28ed578e96d1766f
seunggu <imaster0209@naver.com>
2019-05-06 19:10:47 +0900

    CourseList를 CourseList와 CourseItem으로 분리

---
+++ components/CourseItem.js
@@ -0,0 +1,84 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {
+  StyleSheet,
+  Text,
+  TouchableWithoutFeedback,
+  View,
+} from 'react-native';
+
+import SeperatedText from '../components/SeperatedText';
+
+const propTypes = {
+  course: PropTypes.shape({
+    className: PropTypes.string.isRequired,
+    classDivision: PropTypes.string.isRequired,
+    code: PropTypes.number.isRequired,
+    professorName: PropTypes.string.isRequired,
+    timeTable: PropTypes.string.isRequired,
+  }),
+  onPress: PropTypes.func.isRequired,
+}; 
+
+const CourseItem = ({ course, onPress }) => (
+  <TouchableWithoutFeedback onPress={onPress}>
+    <View style={styles.course}>
+      <View style={styles.coureLeftView}>
+        <Text style={[styles.titleText, styles.bottomPadding]}>{course.className}</Text>
+        <Text style={course.classDivision.includes('전공') ? [styles.redText,styles.bottomPadding] : [styles.grayText,styles.bottomPadding]}>{course.classDivision}</Text>
+        <Text style={[styles.codeText]}>{course.code} 분반</Text>
+        <SeperatedText timeTable={course.timeTable}/>
+      </View >
+      <View style={styles.courseRightView}>
+        <Text style={styles.professorNameText}>{course.professorName} 교수님</Text>
+      </View>
+    </View>
+  </TouchableWithoutFeedback>
+);
+
+const styles = StyleSheet.create({
+  course: {
+    flexDirection: 'row',
+    alignItems: 'stretch',
+    padding: 16,
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: 'skyblue',
+    marginLeft: 10,
+    marginRight: 10,
+    marginBottom: 10,
+  },
+  coureLeftView: {
+    flex: 3,
+    flexDirection: 'column',
+  },
+  courseRightView:{
+    flex: 1,
+
+  },
+  professorNameText: {
+    fontSize: 13,
+  },
+  titleText: {
+    fontWeight: '600',
+    fontSize: 19,
+  },
+  bottomPadding: {
+    paddingBottom : 16,
+  },
+  redText: {
+    color:  'red',
+  },
+  grayText: {
+    color:  'gray',
+  },
+  codeText: {
+    color : 'gray',
+    paddingBottom: 4,
+  },
+});
+
+
+CourseItem.propTypes = propTypes;
+
+export default CourseItem;
--- components/CourseList.js
+++ components/CourseList.js
@@ -1,14 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {
-  StyleSheet,
-  Text,
-  FlatList,
-  TouchableWithoutFeedback,
-  View,
-} from 'react-native';
+import { FlatList } from 'react-native';
 
-import SeperatedText from '../components/SeperatedText';
+import CourseItem from './CourseItem';
 
 const propTypes = {
   courses: PropTypes.arrayOf(PropTypes.shape({
@@ -28,68 +22,12 @@ function CourseList({ courses, onPress }) {
   return (
     <FlatList
       data={courses.map(course => ({ ...course, key: course.id.toString() }))}
-      renderItem={({ item }) => (
-        <TouchableWithoutFeedback onPress={onPress}>
-          <View style={styles.course}>
-            <View style={styles.coureLeftView}>
-              <Text style={[styles.titleText, styles.bottomPadding]}>{item.className}</Text>
-              <Text style={item.classDivision.includes('전공') ? [styles.redText,styles.bottomPadding] : [styles.grayText,styles.bottomPadding]}>{item.classDivision}</Text>
-              <Text style={[styles.codeText]}>{item.code} 분반</Text>
-              <SeperatedText timeTable={item.timeTable}/>
-            </View >
-            <View style={styles.courseRightView}>
-              <Text style={styles.professorNameText}>{item.professorName} 교수님</Text>
-            </View>
-          </View>
-        </TouchableWithoutFeedback>
-      )}
+      renderItem={({ item }) => <CourseItem course={item} onPress={onPress} /> }
     />
   );
 }
 
-
-const styles = StyleSheet.create({
-  course: {
-    flexDirection: 'row',
-    alignItems: 'stretch',
-    padding: 16,
-    borderRadius: 5,
-    borderWidth: 1,
-    borderColor: 'skyblue',
-    marginLeft: 10,
-    marginRight: 10,
-    marginBottom: 10,
-  },
-  coureLeftView: {
-    flex: 3,
-    flexDirection: 'column',
-  },
-  courseRightView:{
-    flex: 1,
-
-  },
-  professorNameText: {
-    fontSize: 13,
-  },
-  titleText: {
-    fontWeight: '600',
-    fontSize: 19,
-  },
-  bottomPadding: {
-    paddingBottom : 16,
-  },
-  redText: {
-    color:  'red',
-  },
-  grayText: {
-    color:  'gray',
-  },
-  codeText: {
-    color : 'gray',
-    paddingBottom: 4,
-  },
-});
-
 CourseList.propTypes = propTypes;
 CourseList.defaultProps = defaultProps;
+
 export default CourseList;
