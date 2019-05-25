cbad77e2a6eabcf1b54af87efac8309762c664d3
Taehwan Noh <taehwanno.dev@gmail.com>
2019-05-25 17:29:12 +0900

    Update token-related implementation

--- screens/AuthLoadingScreen.js
+++ screens/AuthLoadingScreen.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import {
   ActivityIndicator,
   StyleSheet,
   View,
 } from 'react-native';
+import { connect } from 'react-redux';
 import AsyncStorage from '@react-native-community/async-storage';
+
+import { changeUserToken } from '../state/user/actions';
+
+const propTypes = {
+  onTokenChange: PropTypes.func,
+};
+
+const defaultProps = {
+  onTokenChange() {},
+};
 
 class AuthLoadingScreen extends React.Component {
   constructor(props) {
@@ -14,7 +26,12 @@ class AuthLoadingScreen extends React.Component {
 
   bootstrapAsync = async () => {
     const userToken = await AsyncStorage.getItem('userToken');
-    this.props.navigation.navigate(userToken ? 'Main' : 'Auth');
+    if (userToken) {
+      this.props.onTokenChange(userToken);
+      this.props.navigation.navigate('Main', { userToken });
+    } else {
+      this.props.navigation.navigate('Auth');
+    }
   };
 
   render() {
@@ -26,6 +43,9 @@ class AuthLoadingScreen extends React.Component {
   }
 }
 
+AuthLoadingScreen.propTypes = propTypes;
+AuthLoadingScreen.defaultProps = defaultProps;
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -33,4 +53,8 @@ const styles = StyleSheet.create({
   },
 });
 
-export default AuthLoadingScreen;
+const mapDispatchToProps = {
+  onTokenChange: changeUserToken,
+};
+
+export default connect(null, mapDispatchToProps)(AuthLoadingScreen);
